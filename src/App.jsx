c10a7dd4d6b1b12e179ce4d381d0ca55beacd86f
Abhishek Badole <abhishekbadole8@gmail.com>
2023-10-26 18:04:44 +0530

    login create

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+
+        <div className="login">
+
+          <h3>Sign In</h3>
+          
+          <div>
+            <p>Please enter your mobile number to login. We will send an OTP to verify your number.</p>
+            <input type="tel" name="" id="" />
+          </div>
+
+          <button>Sign In</button>
+        </div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
